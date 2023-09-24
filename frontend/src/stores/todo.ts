b978d42f031d797b310a9d503501bcd3affe7227
Maxime Deus <maxime.deroissart@mail.com>
2023-09-24 23:59:33 +0200

    #34 (front/UX) update Todo store, wrap in a try/catch block all actions containing API request, display ErrorAlert component with a custom message if any error

--- frontend/src/stores/todo.ts
+++ frontend/src/stores/todo.ts
@@ -2,12 +2,20 @@ import {ref} from "vue";
 import {defineStore} from "pinia";
 import customAxios from "@/axios";
 import type {TodoProperties} from "@/types/TodoProperties";
+import {ErrorMessages} from "@/types/errorMessages";
+
 export const useTodoStore = defineStore('todo', () => {
     const todos = ref<TodoProperties[]>([]);
+    const errorMessage = ref("");
+
     async function loadTodos() {
-        const {data} : {data: TodoProperties[]} = await customAxios.get("api/todos");
-        if (data){
-            data.forEach((todo: TodoProperties) => todos.value.push(todo));
+        try {
+            const {data}: { data: TodoProperties[] } = await customAxios.get("api/todos");
+            if (data) {
+                data.forEach((todo: TodoProperties) => todos.value.push(todo));
+            }
+        } catch (error) {
+            errorMessage.value = ErrorMessages.LOAD_TODOLIST;
         }
     }
 
@@ -17,25 +25,38 @@ export const useTodoStore = defineStore('todo', () => {
     }
 
     async function addTodo(description: string) {
-        const {data: todo} : {data: TodoProperties} = await customAxios.post("api/todos", {
-            description,
-        });
-        if (todo){
-            todos.value.push(todo);
+        try {
+            const {data: todo}: { data: TodoProperties } = await customAxios.post("api/todos", {
+                description,
+            });
+            if (todo) {
+                todos.value.push(todo);
+            }
+        } catch (error) {
+            errorMessage.value = ErrorMessages.ADD_TODO;
         }
     }
 
     async function removeTodo(id: number) {
-        await customAxios.delete(`api/todos/${id}`);
-        todos.value = todos.value.filter(todo => todo.id !== id);
+        try {
+            await customAxios.delete(`api/todos/${id}`);
+            todos.value = todos.value.filter(todo => todo.id !== id);
+        } catch (error) {
+            errorMessage.value = ErrorMessages.REMOVE_TODO;
+        }
     }
 
-    async function updateTodo(todo : TodoProperties) {
-        const {id, ...rest} = todo;
-        await customAxios.patch(`api/todos/${todo.id}`, rest);
-        const todoIndex = todos.value.findIndex(t => t.id === todo.id);
-        todos.value[todoIndex] = todo;
+    async function updateTodo(todo: TodoProperties) {
+        try {
+            const {id, ...rest} = todo;
+            await customAxios.patch(`api/todos/${todo.id}`, rest);
+            const todoIndex = todos.value.findIndex(t => t.id === todo.id);
+            todos.value[todoIndex] = todo;
+        } catch
+            (error) {
+            errorMessage.value = ErrorMessages.UPDATE_TODO;
+        }
     }
 
-    return { todos, loadTodos, getTodo,  addTodo, removeTodo, updateTodo }
+    return {todos, errorMessage, loadTodos, getTodo, addTodo, removeTodo, updateTodo}
 })
