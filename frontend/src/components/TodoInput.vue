327f0efadf0014f1a369462274ff4d6b737a5d31
Maxime Deus <maxime.deroissart@mail.com>
2023-09-24 16:43:28 +0200

    #26 (front/refacto) create TodoInput component, will be used for refactoring v-text-field on Todo and Todolist components, also create Enum for hint messages

---
+++ frontend/src/components/TodoInput.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-text-field
+      style="width: 300px"
+      v-model="description"
+      :rules="[rules.required]"
+      variant="solo"
+      counter="16"
+      placeholder="Faire les courses"
+      maxlength="16"
+      :hint="hintMessage"
+      @update:focused="updateHintMessageOnFocus"
+      @keyup.enter="validate">
+  </v-text-field>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TodoInput"
+}
+
+</script>
+
+<script setup lang="ts">
+import {ref, watch} from "vue";
+import {HintMessages} from "@/types/hintMessages";
+import {onBeforeMount} from "vue";
+
+const hintMessage = ref<HintMessages>(HintMessages.HIDDEN);
+const description = ref<string>("");
+const props = defineProps({
+  latestDescription: {type: String, default: ""},
+  onSuccess: Boolean,
+});
+
+
+const emit = defineEmits<{
+  (e: 'validate', value: string): void
+}>()
+
+onBeforeMount(async () => {
+  description.value = props.latestDescription;
+})
+
+function updateHintMessageOnFocus(isFocused: boolean) {
+  hintMessage.value = isFocused ? HintMessages.INFO : HintMessages.HIDDEN;
+}
+
+const rules = ref({
+  required: (value: string) => !!value || 'Veuillez renseigner la tâche',
+})
+
+function validate() {
+  if (description.value &&
+      description.value !== "" &&
+      description.value !== props.latestDescription) {
+    emit('validate', description.value)
+  }
+}
+
+watch(() => props.onSuccess, (isSuccess) => {
+  hintMessage.value = HintMessages.HIDDEN;
+  if (isSuccess) {
+    hintMessage.value = HintMessages.UPDATE_SUCCESS;
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
