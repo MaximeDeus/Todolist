996f755292f821052d32bf006adc97bb976c895e
Maxime Deus <maxime.deroissart@mail.com>
2023-09-25 18:27:54 +0200

    #36 (front/refacto) update App and Todolist components, move v-progress-circular and getTodos from Todolist to App (for better error handling)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,34 @@
-<script setup lang="ts">
-import TodolistItem from "@/components/Todolist.vue";
-import CreateTodo from "@/components/createTodo.vue";
-import ErrorAlert from "@/components/ErrorAlert.vue";
-</script>
-
 <template>
   <v-app>
     <v-main class="container">
       <error-alert/>
-      <todolist-item/>
-      <create-todo/>
+      <v-progress-circular v-if="!isLoaded" indeterminate color="blue"/>
+      <template v-else-if="isLoaded && errorMessage !== ErrorMessages.LOAD_TODOLIST"> <!-- fatal error -->
+        <todolist-item/>
+        <create-todo/>
+      </template>
     </v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+import TodolistItem from "@/components/Todolist.vue";
+import CreateTodo from "@/components/createTodo.vue";
+import ErrorAlert from "@/components/ErrorAlert.vue";
+import {useTodoStore} from "@/stores/todo";
+import {storeToRefs} from "pinia";
+import {ErrorMessages} from "@/types/errorMessages";
+import {onBeforeMount, ref} from "vue";
+
+const store = useTodoStore();
+const {errorMessage} = storeToRefs(store);
+const isLoaded = ref(false);
+
+onBeforeMount(async () => {
+  await store.loadTodos();
+  isLoaded.value = true;
+})
+</script>
 
 <style scoped>
 .container {
